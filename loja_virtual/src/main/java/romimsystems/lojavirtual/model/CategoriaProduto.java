c00884187e46c90bd24b601daa89e3a449fbda64
rpinto5 <rspinto1990@gmail.com>
2024-11-14 11:00:37 -0300

    Classe CategoriaProduto

---
+++ loja_virtual/src/main/java/romimsystems/lojavirtual/model/CategoriaProduto.java
@@ -0,0 +1,27 @@
+package romimsystems.lojavirtual.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categoria_produto")
+@SequenceGenerator(name="seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	private Long id;
+	
+	@Column(name = "nome_desc", nullable = false)
+	private String nomeDesc;
+
+}
