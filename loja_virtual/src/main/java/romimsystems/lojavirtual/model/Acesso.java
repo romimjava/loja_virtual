a7bd146938255c0ec0df0b308822ec6c429a33de
rpinto5 <rpinto5@BR-PF1C477S>
2024-11-18 14:35:33 -0300

    Implementação classes model

---
+++ loja_virtual/src/main/java/romimsystems/lojavirtual/model/Acesso.java
@@ -0,0 +1,67 @@
+package romimsystems.lojavirtual.model;
+
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+	private long id;
+
+	@Column(nullable = false)
+	private String descricao; // Exemplo: ROLE_ADMIN, ROLE_USUARIO
+
+	@Override
+	public String getAuthority() {
+		return this.descricao;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Acesso other = (Acesso) obj;
+		return id == other.id;
+	}
+
+}
--- loja_virtual/src/main/java/romimsystems/lojavirtual/model/CategoriaProduto.java
+++ loja_virtual/src/main/java/romimsystems/lojavirtual/model/CategoriaProduto.java
@@ -1,6 +1,7 @@
 package romimsystems.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -24,4 +25,38 @@ public class CategoriaProduto implements Serializable{
 	@Column(name = "nome_desc", nullable = false)
 	private String nomeDesc;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeDesc() {
+		return nomeDesc;
+	}
+
+	public void setNomeDesc(String nomeDesc) {
+		this.nomeDesc = nomeDesc;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaProduto other = (CategoriaProduto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
 }
